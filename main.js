b45d3999c58ad1fb5cb847ef2976d32c83b1b994
Ahmed Murtaza <ahmed.murtaza@ovrlod.com>
2018-02-20 13:08:18 +0500

    mvc structure

---
+++ main.js
@@ -0,0 +1,27 @@
+(function(){
+
+    // Model
+    let data = {
+        lastId:0,
+        pizzas:[]
+    }
+
+    // Controller
+    let controller = {
+        addPizza:function(){},
+        removePizza:function(){},
+        getVisiblePizzas:function(){},
+    }
+
+
+    // View
+    let view = {
+        init:function(){
+            this.render();
+        },
+        render:function(){}
+    }
+
+
+
+})
